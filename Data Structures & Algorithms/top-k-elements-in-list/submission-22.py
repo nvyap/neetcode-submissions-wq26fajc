c4a5219624727951536505387facf22f31c45079
nvyap <35848502+nvyap@users.noreply.github.com>
2026-03-31 12:16:19 -0700

    Add: top-k-elements-in-list - submission-22

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-22.py
@@ -0,0 +1,29 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hm1 = {}
+        hm2 = {}
+        res = []
+
+        for i in range(len(nums)):
+            hm1[nums[i]] = hm1.get(nums[i],0)+1
+        
+        for ind,val in hm1.items():
+            if val not in hm2:
+                hm2[val] = []
+            hm2[val].append(ind)
+        for j in range(len(nums),0,-1):
+            if j in hm2 and len(res)<k:
+                res += hm2[j]
+        return res
+
+
+                
+            
+
+
+        
+
+        
+        
+            
+        
