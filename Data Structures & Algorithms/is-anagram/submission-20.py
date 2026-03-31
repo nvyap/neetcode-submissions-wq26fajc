8a0307ad66ef3c4fa2b049b858242aa158c8c658
nvyap <35848502+nvyap@users.noreply.github.com>
2026-03-31 11:05:12 -0700

    Add: is-anagram - submission-20

---
+++ Data Structures & Algorithms/is-anagram/submission-20.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hm_s = {}
+        hm_t = {}
+        if len(s)!= len(t):
+            return False
+        for i in range(len(s)):
+            hm_s[s[i]] = hm_s.get(s[i],0)+1
+            hm_t[t[i]] = hm_t.get(t[i],0)+1
+        for ind, val in hm_s.items():
+            if val!=hm_t.get(ind,0):
+                return False
+        return True
+
+
+        
+        
+
+            
+        
