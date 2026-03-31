17d5338857ee4fae9e9059dc72cea7536c350038
nvyap <35848502+nvyap@users.noreply.github.com>
2026-03-31 11:26:19 -0700

    Add: two-integer-sum - submission-28

---
+++ Data Structures & Algorithms/two-integer-sum/submission-28.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {}
+        for ind,val in enumerate(nums):
+            diff = target - val
+            if diff in hm:
+                return [hm[diff],ind]
+            hm[val] = ind
+        return []
