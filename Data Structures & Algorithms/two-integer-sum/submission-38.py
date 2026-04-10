b4f019d69de21025709864832990ea81bded3eb2
nvyap <35848502+nvyap@users.noreply.github.com>
2026-04-10 10:18:13 -0700

    Add: two-integer-sum - submission-38

---
+++ Data Structures & Algorithms/two-integer-sum/submission-38.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_set = range(len(nums))
+        res = []
+        hm = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hm:
+                return [hm[diff],i]
+            hm[nums[i]] = i
+
+        return []
+
+        
