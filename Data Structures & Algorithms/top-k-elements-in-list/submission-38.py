0459577a9e803ecd3a83b95a06797841e9853984
nvyap <35848502+nvyap@users.noreply.github.com>
2026-04-13 09:30:53 -0700

    Add: top-k-elements-in-list - submission-38

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-38.py
@@ -0,0 +1,25 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        count_rev = {}
+        res = []
+        for i in nums:
+            count[i] = count.get(i,0)+1
+        
+        for ind,val in count.items():
+            if val not in count_rev:
+                count_rev[val] = []
+            count_rev[val].append(ind)
+        
+        for j in range(len(nums),0,-1):
+            if j in count_rev and len(res)<k:
+                res.extend(count_rev[j])
+
+        return res[:k]
+
+
+        
+
+
+
+        
