c1a4b067e73c479ac2e2b490bd1f6373605198b0
nvyap <35848502+nvyap@users.noreply.github.com>
2026-05-21 10:58:12 -0700

    Add: duplicate-integer - submission-36

---
+++ Data Structures & Algorithms/duplicate-integer/submission-36.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        dup_set = set()
+        for i in nums:
+            if i in dup_set:
+                return True
+            dup_set.add(i)
+        return False
+        
