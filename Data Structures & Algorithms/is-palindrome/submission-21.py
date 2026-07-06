bdd9e50d9287940580f42e00d1b61542d5da1236
nvyap <35848502+nvyap@users.noreply.github.com>
2026-07-06 13:58:14 -0700

    Add: is-palindrome - submission-21

---
+++ Data Structures & Algorithms/is-palindrome/submission-21.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        right = len(s)-1
+        left = 0
+        while left<right:
+            if not s[left].isalnum(): 
+                left += 1 
+                continue
+            
+            if not s[right].isalnum(): 
+                right -= 1
+                continue
+            
+            if s[left].lower() != s[right].lower(): return False
+
+            left+=1
+            right-=1
+        return True
+
+            
+        
+
+
+        
